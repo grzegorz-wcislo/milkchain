e1d6cc88363f12fc5fa61ef99dc34773fcdac23e
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-04-29 18:48:05 +0200

    Add milk transactions

--- contracts/Milkchain.sol
+++ contracts/Milkchain.sol
@@ -5,6 +5,7 @@ contract Milkchain {
         uint liters;
         address owner;
         uint[] eventIds;
+        uint[] transactionIds;
     }
 
     struct Event {
@@ -13,31 +14,54 @@ contract Milkchain {
     }
 
     enum EventType {
-        CREATED
+        CREATED,
+        UHTED,
+        PASTEURIZED
+    }
+
+    struct Transaction {
+        address seller;
+        uint price;
+        uint timestamp;
     }
 
     mapping (uint => MilkPortion) public milkPortions;
     mapping (uint => Event) public events;
+    mapping (uint => Transaction) public transactions;
 
     uint nextMilkId;
     uint nextEventId;
+    uint nextTransactionId;
 
     constructor() public {
-        nextMilkId = 1;
-        nextEventId = 1;
+        nextMilkId = 0;
+        nextEventId = 0;
+        nextTransactionId = 0;
     }
 
     function addMilkPortion(uint liters) public returns (uint) {
-        milkPortions[nextMilkId] = MilkPortion(liters, msg.sender, new uint[](1));
+        milkPortions[nextMilkId] = MilkPortion(liters, msg.sender, new uint[](0), new uint[](0));
         registerEvent(nextMilkId, EventType.CREATED);
         return nextMilkId++;
     }
 
     function registerEvent(uint milkId, EventType et) public returns (uint) {
         require(milkPortions[milkId].owner == msg.sender, "Milk not owned");
+        require(milkPortions[milkId].eventIds.length == 0 || et != EventType.CREATED, "Cannot create twice");
 
         events[nextEventId] = Event(et, now);
         milkPortions[milkId].eventIds.push(nextEventId);
         return nextEventId++;
+    }
+
+    function sellMilk(uint milkId, address buyer, uint price) public returns (uint) {
+        require(milkPortions[milkId].owner == msg.sender, "Milk not owned");
+
+        milkPortions[milkId].owner = buyer;
+
+        transactions[nextTransactionId] = Transaction(msg.sender, price, now);
+        milkPortions[milkId].transactionIds.push(nextTransactionId);
+
+        return nextTransactionId++;
     }
 }
