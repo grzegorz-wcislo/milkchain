54ee57d7fd9d78ca13172056d0fdefd8dec1d5c7
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-04-29 18:17:50 +0200

    Add event log

--- contracts/Milkchain.sol
+++ contracts/Milkchain.sol
@@ -4,17 +4,40 @@ contract Milkchain {
     struct MilkPortion {
         uint liters;
         address owner;
+        uint[] eventIds;
+    }
+
+    struct Event {
+        EventType eventType;
+        uint timestamp;
+    }
+
+    enum EventType {
+        CREATED
     }
 
     mapping (uint => MilkPortion) public milkPortions;
+    mapping (uint => Event) public events;
 
-    uint nextId;
+    uint nextMilkId;
+    uint nextEventId;
 
     constructor() public {
-        nextId = 0;
+        nextMilkId = 1;
+        nextEventId = 1;
     }
 
-    function addMilkPortion(uint liters) public {
-        milkPortions[nextId++] = MilkPortion(liters, msg.sender);
+    function addMilkPortion(uint liters) public returns (uint) {
+        milkPortions[nextMilkId] = MilkPortion(liters, msg.sender, new uint[](1));
+        registerEvent(nextMilkId, EventType.CREATED);
+        return nextMilkId++;
+    }
+
+    function registerEvent(uint milkId, EventType et) public returns (uint) {
+        require(milkPortions[milkId].owner == msg.sender, "Milk not owned");
+
+        events[nextEventId] = Event(et, now);
+        milkPortions[milkId].eventIds.push(nextEventId);
+        return nextEventId++;
     }
 }
