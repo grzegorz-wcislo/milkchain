eb1fc4ca1abb313d7c528ef6bb0cf0d22e762961
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-04-29 17:52:26 +0200

    Almost done

--- contracts/Milkchain.sol
+++ contracts/Milkchain.sol
@@ -1,4 +1,20 @@
 pragma solidity ^0.6.4;
 
 contract Milkchain {
+    struct MilkPortion {
+        uint liters;
+        address owner;
+    }
+
+    mapping (uint => MilkPortion) public milkPortions;
+
+    uint nextId;
+
+    constructor() public {
+        nextId = 0;
+    }
+
+    function addMilkPortion(uint liters) public {
+        milkPortions[nextId++] = MilkPortion(liters, msg.sender);
+    }
 }
